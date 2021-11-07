ef562d4cd1d0f56551b59df3345fc44e78da7444
RafaelRufino <rafaelrufino.52@gmail.com>
2021-11-07 01:19:48 -0300

    criando as routas de agendamento, (GET, POST)

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+const appointmentsRouter = Router();
+
+const appointments: Appointment[] = [];
+
+appointmentsRouter.get('/', (req, res) => {
+  return res.json({ appointments });
+});
+
+appointmentsRouter.post('/', (req, res) => {
+  const { provider, date } = req.body;
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+  if (findAppointmentInSameDate) {
+    return res
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = new Appointment(provider, parsedDate);
+  appointments.push(appointment);
+
+  return res.json(appointment);
+});
+
+export default appointmentsRouter;
